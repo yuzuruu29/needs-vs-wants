a14e596f265790273aff284d51a1db9c6af9671c
Emmanuel Uzziel Malolos <uzzielmalolos@gmail.com>
2026-08-29 01:49:26 +0800

    release: ship version 2.0.24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.needsvswants.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 31
-        versionName = "2.0.23"
+        versionCode = 32
+        versionName = "2.0.24"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         // --- Google Play Billing Subscriptions -----------------------------
